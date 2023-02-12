88c99f52dee69c939ece7a52c300a37b181ca3cf
Encryptedknox <100846845+Encryptedknox@users.noreply.github.com>
2023-02-12 09:38:31 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -82,7 +82,7 @@ async def start(bot: Client, cmd: Message):
                     ],
                     [
                         InlineKeyboardButton("Bots Channel", url="https://t.me/knoxprojects"),
-                        InlineKeyboardButton(" Support Group", url="https://t.me/knoxbotsgroup")
+                        InlineKeyboardButton(" Support Group", url="https://t.me/knoxsupport")
                     ]
                 ]
             )
@@ -123,7 +123,7 @@ async def main(bot: Client, message: Message):
                 return
 
         if message.from_user.id in Config.BANNED_USERS:
-            await message.reply_text("Sorry, You are banned!\n\nContact [Support Group](https://t.me/knoxbotsgroup)",
+            await message.reply_text("Sorry, You are banned!\n\nContact [Support Group](https://t.me/knoxsupport)",
                                      disable_web_page_preview=True)
             return
 
@@ -361,7 +361,7 @@ async def button(bot: Client, cmd: CallbackQuery):
                         InlineKeyboardButton("Close ", callback_data="closeMessage")
                     ],
                     [
-                        InlineKeyboardButton("Support Group", url="https://t.me/knoxbotsgroup"),
+                        InlineKeyboardButton("Support Group", url="https://t.me/knoxsupport"),
                         InlineKeyboardButton("Bots Channel", url="https://t.me/knoxprojects")
                     ]
                 ]
@@ -378,7 +378,7 @@ async def button(bot: Client, cmd: CallbackQuery):
                 user = await bot.get_chat_member(channel_chat_id, cmd.message.chat.id)
                 if user.status == "kicked":
                     await cmd.message.edit(
-                        text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/knoxbotsgroup).",
+                        text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/knoxsupport).",
                         disable_web_page_preview=True
                     )
                     return
@@ -400,7 +400,7 @@ async def button(bot: Client, cmd: CallbackQuery):
                 return
             except Exception:
                 await cmd.message.edit(
-                    text="Something went Wrong. Contact my [Support Group](https://t.me/knoxbotsgroup).",
+                    text="Something went Wrong. Contact my [Support Group](https://t.me/knoxsupport).",
                     disable_web_page_preview=True
                 )
                 return
@@ -410,7 +410,7 @@ async def button(bot: Client, cmd: CallbackQuery):
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("Support Group", url="https://t.me/knoxbotsgroup"),
+                        InlineKeyboardButton("Support Group", url="https://t.me/knoxsupport"),
                         InlineKeyboardButton("Bots Channel", url="https://t.me/knoxprojects")
                     ],
                     [
